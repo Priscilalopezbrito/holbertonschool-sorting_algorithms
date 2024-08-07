00d91a5454d738cd520e59e544dc91559e1020f1
Priscila Lopez <prisscilalb@gmail.com>
2024-08-07 18:37:41 -0400

    fix print_array function

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,7 +7,7 @@
  **/
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, p, min, temp;
+	unsigned int i, j, min, temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -26,15 +26,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[i];
 			array[i] = array[min];
 			array[min] = temp;
-			for (p = 0; p < size; p++)
-			{
-				if (p != 0)
-				{
-					printf(", ");
-				}
-				printf("%d", array[p]);
-			}
-			printf("\n");
+			print_array(array, size);
 		}
 	}
 }
