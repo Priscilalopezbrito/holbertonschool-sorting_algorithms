cb26ef8cc05d3c220f1cfa8134a248adcdaf624c
Priscila Lopez <prisscilalb@gmail.com>
2024-08-08 19:32:04 -0400

    Adding file 1-insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,7 @@
 #include "sort.h"
+
+void swap(listint_t **list, listint_t *left, listint_t *right);
+
 /**
  * insertion_sort_list- sorts a doubly linked list in ascending
  * order using Insertion sort algorithm
@@ -6,15 +9,48 @@
  **/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *sorted = NULL;
-	listint_t *current = list;
-	while (current != NULL)
+	listint_t *current = (*list)->next;
+	listint_t *next = current->next;
+
+	if (!list || !*list || !(*list)->next)
 	{
-		listint_t *next = current->next;
-		current->prev = current->next = NULL;
-		insertion_sort_list(&sorted, current);
+		return;
+	}
+	while (current)
+	{
+		next = current->next;
+		while (current->prev && current->prev->n > current->n)
+		{
+			swap(list, current->prev, current);
+		}
 		current = next;
 	}
-	*list = sorted;
+}
+
+
+/**
+ * swap- swap nodes
+ * @list: list
+ * @left: left
+ * @right: right
+ **/
+void swap(listint_t **list, listint_t *left, listint_t *right)
+{
+	if (left->prev)
+	{
+		left->prev->next = right;
+	}
+	else
+	{
+		*list = right;
+	}
+	if (right->next)
+	{
+		right->next->prev = left;
+	}
+	left->next = right->next;
+	right->prev = left->prev;
+	left->prev = right;
+	right->next = left;
 	print_list(*list);
 }
